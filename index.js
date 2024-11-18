802b55c9c2d5f19a6cfacb3e7d94c48f224a3f5e
atosamit <amit.3.mishra@atos.net>
2024-11-18 13:31:17 +0530

    Initial commit with schema and resolver

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import routes from './routes/app.route.js'; // Importing routes
 import { ApolloServer } from 'apollo-server-express'; // Import from apollo-server-express
 
 // Import GraphQL type definitions and resolvers
-import demotypeDefs from './graphql/demo.js';
-import demoresolver from './graphql/demoresolver.js';
+import demotypeDefs from './graphql/finalSchema.js';
+import demoresolver from './graphql/finalResolver.js';
 
 const app = express();
 app.use(express.json());
